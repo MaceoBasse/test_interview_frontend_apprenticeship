49187c026e48d9c8b9971cc8215f936dbebe1822
Maceo <basse.maceo@gmail.com>
2024-08-25 14:11:41 +0200

    Add NameList component for rendering list of names

--- react/react-tasks/src/App.js
+++ react/react-tasks/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Greeting from './Greeting';
 import Counter from './Counter';
+import NameList from './NameList';
 
 function App() {
   return (
     <div className="center">
       <Greeting name="Maceo" />
       <Counter />
+      <NameList />
     </div>
   );
 }
